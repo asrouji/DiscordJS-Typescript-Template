c947a9f67ef686890488d2fd78ea62e6e0a1bcfb
Aidan Srouji <72050533+asrouji@users.noreply.github.com>
2023-07-04 19:30:12 -0700

    remove console error if interaction is not chat input command

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -6,25 +6,22 @@ const event = createEvent({
   name: Events.InteractionCreate,
   once: false,
   execute: async interaction => {
-    if (!interaction.isChatInputCommand()) {
-      console.error(`Interaction ${interaction.id} is not a command.`)
-      return
-    }
+    if (interaction.isChatInputCommand()) {
+      // we cast the client to our custom client to read the commands collection
+      const command = (interaction.client as BotClient).commands.get(interaction.commandName)
 
-    // we cast the client to our custom client to read the commands collection
-    const command = (interaction.client as BotClient).commands.get(interaction.commandName)
+      if (!command) {
+        console.error(`Command ${interaction.commandName} not found.`)
+        await interaction.reply({ content: 'Command not found on server!', ephemeral: true })
+        return
+      }
 
-    if (!command) {
-      console.error(`Command ${interaction.commandName} not found.`)
-      await interaction.reply({ content: 'Command not found on server!', ephemeral: true })
-      return
-    }
-
-    try {
-      await command.execute(interaction)
-    } catch (error) {
-      console.error(error)
-      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+      try {
+        await command.execute(interaction)
+      } catch (error) {
+        console.error(error)
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+      }
     }
   },
 })
