e78604e3e238d1f92485b2116e90ba148007d40f
asrouji <72050533+asrouji@users.noreply.github.com>
2023-12-16 20:58:46 -0800

    refactor stringSelectMenu handler to handle specific select menu interactions

--- src/handlers/stringSelectMenu.ts
+++ src/handlers/stringSelectMenu.ts
@@ -7,8 +7,13 @@ dotenv.config()
 const handler: InteractionHandler<StringSelectMenuInteraction> = {
   handle: async interaction => {
     // use interaction.customId to determine which string select menu was used
-    console.error(`Unknown StringSelectMenuInteraction ${interaction.customId}`)
-    interaction.deferUpdate().catch(console.error)
+    if (interaction.customId === 'sample_select_menu') {
+      const selectedCustomId = interaction.values[0]
+      await interaction.update({ content: `Selection ID: ${selectedCustomId}`, components: [] })
+    } else {
+      console.error(`Unknown StringSelectMenuInteraction ${interaction.customId}`)
+      interaction.deferUpdate().catch(console.error)
+    }
   },
 }
 
