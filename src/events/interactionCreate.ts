7e5f8dfe8469c7a902f271963f5fe0deaa016df0
asrouji <72050533+asrouji@users.noreply.github.com>
2023-12-17 02:08:34 -0800

    rename `event.ts` to `eventListener.ts`

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,5 +1,5 @@
 import { Events } from 'discord.js'
-import { createEventListener } from '../types/event'
+import { createEventListener } from '../types/eventListener'
 
 import chatInputCommandHandler from '../handlers/chatInputCommand'
 import stringSelectMenuHandler from '../handlers/stringSelectMenu'
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,5 @@
 import { Events } from 'discord.js'
-import { createEventListener } from '../types/event'
+import { createEventListener } from '../types/eventListener'
 
 const event = createEventListener({
   name: Events.ClientReady,
